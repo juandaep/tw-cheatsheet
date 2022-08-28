227488056cb243e1584b4ba239f54327b7522c0b
juandaep <36181732+juandaep@users.noreply.github.com>
2022-08-28 16:38:51 +0700

    reformat

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,25 +4,25 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-      '13': '3.25rem',
-      '15': '3.75rem',
-      '128': '32rem',
-      '144': '36rem',
-    },
-    fontFamily: {
-      inter: ['Inter']
-    },
-    animation: {
-      goyang: 'goyang 1s ease-in-out infinite'
-    },
-    keyframes: {
-      goyang: {
-        '0%, 100%' : { transform: 'rotate(-3deg)'},
-        '50%' : { transform: 'rotate(3deg)' },
+        13: "3.25rem",
+        15: "3.75rem",
+        128: "32rem",
+        144: "36rem",
       },
-    },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      animation: {
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
     },
   },
   plugins: [],
-  darkMode: 'class'
-}
+  darkMode: "class",
+};
